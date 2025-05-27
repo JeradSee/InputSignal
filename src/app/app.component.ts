3da3c77dca2e3cd01934e1266b73bb351751822b
Jerad <jeradc101@gmail.com>
2025-05-27 13:22:40 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from './list.component';
+import { User } from './models';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [UserListComponent],
+  template: `
+    <section class="header">
+      <button (click)="addUser()">Add Andy</button>
+      <img width="120" src="./assets/logo-01.png" />
+    </section>
+    <app-user-list [users]="users" />
+  `,
 })
+
 export class AppComponent {
-  title = 'input-signals';
-}
+  users: User[] = [
+    { id: 1, name: 'Jerad', lastName: 'Cho', username: 'jerad.cho' },
+    { id: 2, name: 'Brandon', lastName: 'Pasquarelli', username: 'brandon.pasquarelli' },
+    { id: 3, name: 'Stephen', lastName: 'Birchmore', username: 'stephen.birchmore' },
+    { id: 4, name: 'Darrik', lastName: 'Marstaller', username: 'darrik.marstaller' },
+  ];
+
+  addUser() {
+    this.users = [
+      {
+        id: 5,
+        name: 'Brown',
+        lastName: 'Bear',
+        username: 'brown.bear',
+      },
+      ...this.users,
+    ];
+    console.log('Current Users', this.users);
+  }
+};
---
+++ src/app/list.component.ts
@@ -0,0 +1,49 @@
+import { Component, computed, effect, input, signal } from '@angular/core';
+import { User, ModifiedUser } from './models';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  template: `
+    <input (input)="updateQuery($event)" placeholder="Start typing..." />
+    <ul>
+      @for (user of filteredUsers(); track user.id) {
+        <li>{{ user.displayName }}</li>
+      }
+    </ul>
+  `,
+})
+
+export class UserListComponent {
+
+  userList = input.required({
+    alias: 'users',
+    transform: concatUserNames
+  });
+
+  constructor() {
+    effect(() => {
+      // the way we track changes in signals
+      console.log('New Input value is: ', this.userList());
+    })
+  }
+
+  protected filteredUsers = computed(() =>
+    this.userList().filter(({ displayName }) =>
+      displayName.startsWith(this.query())
+    )
+  );
+
+  private query = signal('');
+
+  updateQuery(e: Event) {
+    this.query.set((e.target as HTMLInputElement).value);
+  }
+}
+
+function concatUserNames(users: User[]): ModifiedUser[] {
+  return users.map(({ name, lastName, ...user }) => ({
+    ...user,
+    displayName: `${name} ${lastName}`,
+  }));
+}
